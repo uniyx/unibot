44ff650d1d2959aadb183ffb5142a1882d4d120d
uniyx <uni@esteves.net>
2025-11-24 17:25:25 -0500

    playoffs id

--- cogs/esea.py
+++ cogs/esea.py
@@ -15,7 +15,7 @@ from zoneinfo import ZoneInfo
 # =========================
 OUTPUT_TZ       = "America/New_York"
 TEAM_ID         = "15c9a36f-8169-49eb-a41b-0a0e7567ed37"      # crescent
-CHAMPIONSHIP_ID = "c5749517-d0b9-4d12-aec1-329393db934b"      # ESEA S55 NA Main Central
+CHAMPIONSHIP_ID = "4597d777-3e7b-4c67-b636-55a8f0b537e7"      # ESEA S55 NA Main Central
 
 FACEIT_PUBLIC_V1  = "https://www.faceit.com/api"
 FACEIT_ROOM_BASE  = "https://www.faceit.com/en/cs2/room"
